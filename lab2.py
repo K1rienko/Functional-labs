130eb46d754d9e6f005b134bf92326d3aeea5baf
K1rienko <aleksey.kiriyenko.2012@gmail.com>
2023-02-04 11:39:46 +0600

    lab2

---
+++ lab2.py
@@ -0,0 +1,19 @@
+name = "Alexey" 
+surname = "Kiriyenko"
+patronymic = "Nickolaevich"
+hobby = "Gym"
+
+print("Full name: %s %s %s" % (surname, name, patronymic))
+print("Hobby: %s" % hobby)
+
+vvod = str(input("Do you love me?"))
+ 
+
+#Условные операторы
+if vvod == "yes": 
+    print("I love you, too!")
+elif vvod == "no":
+    print("Ohhh")
+else:
+    print(":)")
+# Это комментарии
